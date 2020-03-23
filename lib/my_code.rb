ffdb2b793791f8c88e1197dbd1c298ff1acf0517
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-23 15:40:06 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
 # Your Code Here
+def map(source)
+  result = []
+  source.length.times do |index|
+    result << yield(source[index])
+  end
+  result
+end
+
+def reduce(source, initial_value = nil)
+  if initial_value
+    acc = initial_value
+    i = 0
+  else
+    acc = source[0]
+    i = 1
+  end
+  
+  while i < source.length do
+    acc = yield(acc, source[i])
+    i += 1
+  end
+  acc
+end
